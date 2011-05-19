0a90a4ee37c8d8e9a30aa38eed6d3ee267318abf
Brandon Lewis <brandon@scattershot.(none)>
2011-05-18 20:01:41 -0400

    I messed up Gradus

--- lib/gradus/add.rb
+++ lib/gradus/add.rb
@@ -1,7 +1,11 @@
 module Gradus 
   class Add
     def prompt()
-      grade = ask 'please insert a grade'
+     grade = ask 'please insert a grade'
+    end
+    def validate()
+     grade.class.name.should == 'Integer'
     end
   end
 end
+
